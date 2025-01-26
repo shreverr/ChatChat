b2cdaf3117c5f7335264b2a5998b18bd0f8e1606
Shreshth Verma <verma2007s@gmail.com>
2025-01-27 04:34:02 +0530

    feat: socket.io init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { SocketModule } from './socket/socket.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -14,7 +16,9 @@ import * as Joi from 'joi';
         MONOGO_DB_URI: Joi.string().required(),
         PORT: Joi.number().port().default(3000),
       }),
-    })
+    }),
+    DatabaseModule,
+    SocketModule
   ],
   controllers: [AppController],
   providers: [AppService],
