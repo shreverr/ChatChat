4a6288b8853e005bfb0643563a86e9397789c1ea
Shreshth Verma <verma2007s@gmail.com>
2025-01-26 22:27:54 +0530

    feat: config module init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    LoggerModule
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONOGO_DB_URI: Joi.string().required(),
+        PORT: Joi.number().port().default(3000),
+      }),
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
